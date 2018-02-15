93fe786f65384d4ac8bf1850fd1fdda9136eb7bb
EmilyW-3 <EmilyW-3@users.noreply.github.com>
2018-02-15 14:26:54 +0000

    LevelLoader

---
+++ Build/nclgl/LevelLoader.cpp
@@ -0,0 +1,140 @@
+#include "LevelLoader.h"
+
+#include <fstream>
+#include <sstream>
+
+#include <ncltech\Scene.h>
+
+using namespace std;
+
+#include <ncltech\CommonUtils.h>
+
+LevelLoader::LevelLoader()
+{
+}
+
+
+LevelLoader::~LevelLoader()
+{
+}
+
+bool LevelLoader::Load(const std::string& filename)
+{
+	string info;
+	ifstream levelfile("SimpleLevel.txt");
+	if (levelfile.is_open()) {
+		cout << "File opened" << endl;
+		while (getline(levelfile, info)) {
+			AddMapObject(info);
+		}
+		levelfile.close();
+
+
+		return true;
+	}
+	else {
+		cout << "File failed to be read" << endl;
+		return false;
+	}
+}
+
+void LevelLoader::AddMapObject(const std::string& line)
+{
+	stringstream ss(line);
+	MapObject obj;
+
+	ss >> obj.name;
+
+	std::string temp;
+	ss >> temp;
+	obj.type = StringToType(temp);
+
+	float x, y, z;
+	ss >> x >> y >> z;
+	obj.position = Vector3(x, y, z);
+
+	ss >> x >> y >> z;
+	obj.scale = Vector3(x, y, z);
+
+	ss >> obj.inverseMass;
+	ss >> obj.rotationDegrees;
+
+	mapObjects.push_back(obj);
+}
+
+Type LevelLoader::StringToType(const std::string& line)
+{
+	if (line == "GEOMETRY_CUBOID")
+	{
+		return GEOMETRY_CUBOID;
+	}
+	else if (line == "GEOMETRY_SPHERE")
+	{
+		return GEOMETRY_SPHERE;
+	}
+	else if (line == "GEOMETRY_RAMP")
+	{
+		return GEOMETRY_RAMP;
+	}
+}
+
+void  LevelLoader::BuildLevel(const std::string& filename, Scene* scene)
+{
+	Load(filename);
+	
+	for (auto& object : mapObjects)
+	{
+		GameObject * geometry;
+		switch (object.type)
+		{
+		case GEOMETRY_CUBOID:
+			geometry = CommonUtils::BuildCuboidObject(
+				object.name,
+				object.position,
+				object.scale,
+				true,
+				object.inverseMass,
+				true,
+				false,
+				Vector4(0.2f, 0.5f, 1.0f, 1.0f));
+
+			scene->AddGameObject(geometry);
+			break;
+		case GEOMETRY_SPHERE:
+			geometry = CommonUtils::BuildSphereObject(
+				object.name,
+				object.position,
+				object.scale.x,
+				true,
+				object.inverseMass,
+				true,
+				false,
+				Vector4(0.2f, 0.5f, 1.0f, 1.0f));
+
+			scene->AddGameObject(geometry);
+			break;
+
+		case GEOMETRY_RAMP:
+			geometry = CommonUtils::BuildRampObject(
+				object.name,
+				object.position,
+				object.scale,
+				true,
+				object.inverseMass,
+				true,
+				false,
+				Vector4(0.2f, 0.5f, 1.0f, 1.0f),
+				Vector3(0.0f, 1.0f, 0.0f),
+				object.rotationDegrees
+				);
+
+			scene->AddGameObject(geometry);
+			break;
+		}
+	}
+
+	scene->OnInitializeScene();
+	mapObjects.clear();
+}
+//
+
---
+++ Build/nclgl/LevelLoader.h
@@ -0,0 +1,45 @@
+#pragma once
+#include <string>
+#include <nclgl\Vector3.h>
+#include <nclgl\Vector4.h>
+
+#include <vector>
+
+
+class Scene;
+
+enum Type {
+	GEOMETRY_CUBOID,
+	GEOMETRY_SPHERE,
+	GEOMETRY_RAMP,
+};
+
+
+class LevelLoader
+{
+	struct MapObject
+	{
+		std::string name;
+		Type		type;
+		Vector3		position;
+		Vector3		scale;
+		float		inverseMass;
+		float		rotationDegrees;
+	};
+
+public:
+	LevelLoader();
+	~LevelLoader();
+
+	void BuildLevel(const std::string& filename, Scene* scene);
+
+protected:
+	void AddMapObject(const std::string& line);
+	bool Load(const std::string& filename);
+
+	std::vector<MapObject> mapObjects;
+
+protected:
+	Type StringToType(const std::string& line);
+};
+
