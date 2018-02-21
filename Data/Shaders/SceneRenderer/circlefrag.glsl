f548e474980de66ffa0cc62425d4f288626d1686
NemDam <damgouzkouris@yahoo.com>
2018-02-21 12:15:58 +0000

    Clockwise Pie

--- Data/Shaders/SceneRenderer/circlefrag.glsl
+++ Data/Shaders/SceneRenderer/circlefrag.glsl
@@ -22,7 +22,10 @@ void main(void)
 			
 			vec2 test = vec2(IN.texCoord.x-0.5, IN.texCoord.y-0.5);
 
-			float Angle = atan((IN.texCoord.y-0.5),(IN.texCoord.x-0.5))+PI;
+			float Angle = atan((IN.texCoord.y-0.5),(IN.texCoord.x-0.5))-PI/2;
+			if (Angle<0) Angle+=2*PI;
+			Angle = 2*PI - Angle;
+
 			for (int i=0; i<num_players-1; i++){	
 				if(wr==-1 && Angle <= players[i].angle){
 					wr = i;
